929b3d667a685fb1fd025c3cc93b40c05b70964c
Krystsina Shpileuskaya <kainsha@gmail.com>
2022-04-28 12:51:27 -0500

    done

--- preguntas.py
+++ preguntas.py
@@ -118,36 +118,38 @@ def pregunta_04():
     # Importe LinearRegression
     # Importe train_test_split
     # Importe mean_squared_error
-    from ____ import ____
+    from sklearn.linear_model import LinearRegression
+    from sklearn.model_selection import train_test_split
+    from sklearn.metrics import mean_squared_error
 
     # Lea el archivo `gm_2008_region.csv` y asignelo al DataFrame `df`
-    df = ____
+    df = pd.read_csv("gm_2008_region.csv")
 
     # Asigne a la variable los valores de la columna `fertility`
-    X_fertility = ____
+    X_fertility = df["fertility"].values.reshape(139, 1)
 
     # Asigne a la variable los valores de la columna `life`
-    y_life = ____
+    y_life = df["life"].values.reshape(139, 1)
 
     # Divida los datos de entrenamiento y prueba. La semilla del generador de números
     # aleatorios es 53. El tamaño de la muestra de entrenamiento es del 80%
-    (X_train, X_test, y_train, y_test,) = ____(
-        ____,
-        ____,
-        test_size=____,
-        random_state=____,
+    (X_train, X_test, y_train, y_test,) = train_test_split(
+        X_fertility,
+        y_life,
+        test_size=0.2,
+        random_state=53,
     )
 
     # Cree una instancia del modelo de regresión lineal
-    linearRegression = ____
+    linearRegression = LinearRegression()
 
     # Entrene el clasificador usando X_train y y_train
-    ____.fit(____, ____)
+    linearRegression.fit(X_train, y_train)
 
     # Pronostique y_test usando X_test
-    y_pred = ____
+    y_pred = linearRegression.predict(X_test)
 
     # Compute and print R^2 and RMSE
     print("R^2: {:6.4f}".format(linearRegression.score(X_test, y_test)))
-    rmse = np.sqrt(____(____, ____))
+    rmse = np.sqrt(mean_squared_error(y_test, y_pred))
     print("Root Mean Squared Error: {:6.4f}".format(rmse))
